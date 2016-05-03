87eceb5171b55f26a8c3145782762a6d1d9fe0de
Michał Walenciak <Kicer86@gmail.com>
2016-05-03 11:50:48 +0200

    introducing ObjectsScene

---
+++ src/objects_scene.hpp
@@ -0,0 +1,34 @@
+/*
+ * Scene for graviting objects
+ * Copyright (C) 2016  Michał Walenciak <MichalWalenciak@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef OBJECTSSCENE_HPP
+#define OBJECTSSCENE_HPP
+
+#include <QGraphicsScene>
+
+class ObjectsScene: public QGraphicsScene
+{
+    public:
+        ObjectsScene();
+        ObjectsScene(const ObjectsScene &) = delete;
+        ~ObjectsScene();
+        ObjectsScene& operator=(const ObjectsScene &) = delete;
+};
+
+#endif // OBJECTSSCENE_HPP
