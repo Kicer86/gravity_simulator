ba47d5ad6ae83daa4ef8bab5bbe806684790dbef
Michał Walenciak <Kicer86@gmail.com>
2016-05-03 13:10:29 +0200

    MainWindow: setting view

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -29,6 +29,11 @@ MainWindow::MainWindow(): m_scene(), m_controller()
 
     ui->graphicsView->setScene(&m_scene);
     m_controller.setScene(&m_scene);
+
+    m_controller.beginSimulation();
+
+    auto rect = m_scene.sceneRect();
+    ui->graphicsView->fitInView(rect);
 }
 
 
