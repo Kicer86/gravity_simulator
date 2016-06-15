768394543862d9637c064d35eae95a3e78bf15bc
Michał Walenciak <Kicer86@gmail.com>
2016-06-15 20:52:21 +0200

    SimulationController: duping simulation speed to output

--- src/simulation_controller.cpp
+++ src/simulation_controller.cpp
@@ -165,9 +165,17 @@ int SimulationController::fps() const
 
 void SimulationController::tick()
 {
+    const std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+
     m_tickData.clear();
     m_engine.stepBy(180);
     m_framesCounter++;
+
+    const std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    const auto diff = end - start;
+    const auto diff_ms = std::chrono::duration_cast<std::chrono::milliseconds>(diff).count();
+
+    std::cout << "Tick time: " << diff_ms << std::endl;
 
     emit tickData(m_tickData);
 }
